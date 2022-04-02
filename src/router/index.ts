09084326fac115974807f0f04194add4422eb1c5
HWJ-Code <1761481088@qq.com>
2022-04-02 14:44:44 +0800

    feat: 验证token

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
+import localCache from '@/utils/cache'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -21,4 +22,12 @@ const router = createRouter({
   history: createWebHashHistory()
 })
 
+router.beforeEach((to) => {
+  const token = localCache.getCache('token')
+  if (to.path != '/login' && !token) {
+    //路由跳转验证token 没有token跳到登录页
+    router.push('/login')
+  }
+  console.log(to)
+})
 export default router
